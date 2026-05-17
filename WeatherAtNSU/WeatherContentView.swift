6e1a4bf57afcdab82edb3f7ba2357b6557562154
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2026-05-18 00:07:34 +0700

    Поправки.

--- WeatherAtNSU/WeatherContentView.swift
+++ WeatherAtNSU/WeatherContentView.swift
@@ -24,72 +24,69 @@ struct WeatherContentView: View {
     @State var degrees: Double = 0
     
     var body: some View {
-        ZStack {
-            Image(self.image)
-            .resizable()
-            .scaledToFill()
-            .frame(height: UIScreen.main.bounds.height)
-            .edgesIgnoringSafeArea(.top)
+        VStack {
+            Spacer(minLength: Constants.weatherAtLabelInset)
             
-            VStack {
-                Spacer(minLength: Constants.weatherAtLabelInset)
-                
-                let bigFont = Font.system(size: 60, weight: .semibold)
-                let textColor = Color.white
-                
-                Text(Constants.weatherAtLabel).foregroundColor(textColor).font(.system(size: 14, weight: .semibold))
-                Text(self.location).foregroundColor(textColor).font(bigFont)
-                
-                Spacer()
-                
-                Text(self.weather).foregroundColor(textColor).font(bigFont)
-                
-                Spacer()
-                
-                let updateWeatherButtonSize = Constants.updateWeatherButtonSize
-                Button(action: {
-                    self.degrees += 360
-                    Task {
-                        do {
-                            self.weather = try await self.weatherLoader.loadWeather()
-                        } catch {
-                            print(error)
-                            self.weather = "?"
-                        }
+            let bigFont = Font.system(size: 60, weight: .semibold)
+            let textColor = Color.white
+            
+            Text(Constants.weatherAtLabel).foregroundColor(textColor).font(.system(size: 14, weight: .semibold))
+            Text(self.location).foregroundColor(textColor).font(bigFont)
+            
+            Spacer()
+            
+            Text(self.weather).foregroundColor(textColor).font(bigFont)
+            
+            Spacer()
+            
+            let updateWeatherButtonSize = Constants.updateWeatherButtonSize
+            
+            Button(action: {
+                self.degrees += 360
+                Task {
+                    do {
+                        self.weather = try await self.weatherLoader.loadWeather()
+                    } catch {
+                        print(error)
+                        self.weather = "?"
                     }
-                }, label: {
-                    Image(Constants.updateWeatherImageName)
-                        .rotationEffect(.degrees(self.degrees))
-                        .animation(.easeInOut(duration: 0.35))
-                })
-                .frame(width: updateWeatherButtonSize, height: updateWeatherButtonSize)
-                .background(Color.black.opacity(0.5)).cornerRadius(updateWeatherButtonSize / 2)
-                
-                Spacer().frame(height: Constants.updateWeatherButtonBottomSpace)
-                
-                HStack(spacing: 0, content: {
-                    let smallFont = Font.system(size: 14, weight: .semibold)
-                    Text(Constants.poweredByLabel).foregroundColor(textColor).font(smallFont)
-                    Button(Constants.linkButtonLabel, action: {
-                        self.linkButtonDisabled = true
-                        UIApplication.shared.open(Constants.githubURL, options: [:], completionHandler: { _ in
-                            self.linkButtonDisabled = false
-                        })
-                    }).font(smallFont).disabled(self.linkButtonDisabled)
-                })
-                
-                Spacer().frame(height: Constants.linksViewBottomInset + 50)
+                }
+            }, label: {
+                Image(Constants.updateWeatherImageName)
+                    .rotationEffect(.degrees(self.degrees))
+                    .animation(.easeInOut(duration: 0.35), value: self.degrees)
+            })
+            .frame(width: updateWeatherButtonSize, height: updateWeatherButtonSize)
+            .background(Color.black.opacity(0.5)).cornerRadius(updateWeatherButtonSize / 2)
+            
+            Spacer().frame(height: Constants.updateWeatherButtonBottomSpace)
+            
+            HStack(spacing: 0, content: {
+                let smallFont = Font.system(size: 14, weight: .semibold)
+                Text(Constants.poweredByLabel).foregroundColor(textColor).font(smallFont)
+                Button(Constants.linkButtonLabel, action: {
+                    self.linkButtonDisabled = true
+                    UIApplication.shared.open(Constants.githubURL, options: [:], completionHandler: { _ in
+                        self.linkButtonDisabled = false
+                    })
+                }).font(smallFont).disabled(self.linkButtonDisabled)
+            })
+            
+            Spacer().frame(height: Constants.linksViewBottomInset + 50)
+        }
+        .background {
+            Image(self.image)
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.top)
+        }
+        .task {
+            do {
+                self.weather = try await self.weatherLoader.loadWeather()
+            } catch {
+                print(error)
+                self.weather = "?"
             }
         }
-        .onAppear(perform: {
-            Task {
-                do {
-                    self.weather = try await self.weatherLoader.loadWeather()
-                } catch {
-                    print(error)
-                    self.weather = "?"
-                }
-            }
-        })
     }
 }
